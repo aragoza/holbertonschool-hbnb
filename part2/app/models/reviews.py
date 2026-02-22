5feadec24e699bdc5cf88b2b22d9258b1881dcb2
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-22 18:42:19 +0100

    feat: Added Review model

---
+++ part2/app/models/reviews.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from __init__ import BaseModel
+from places import Place
+from users import User
+
+class Review(BaseModel):
+    def __init__(self, text: str, rating: int, place: Place, user: User):
+        super().__init__()
+        self.text = text
+        self.rating = rating
+        self.place = place
+        self.user = user
