c2f688bf2009eb4583e18e0aa765713656a4f1c8
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-22 18:50:30 +0100

    fix: Added type hint

--- part2/app/models/places.py
+++ part2/app/models/places.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 from __init__ import BaseModel
+from amenities import Amenity
+from reviews import Review
+from users import User
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, title: str, description: str, price: float, latitude: float, longitude: float, owner: User):
         super().__init__()
         self.title = title
         self.description = description
@@ -14,10 +17,10 @@ class Place(BaseModel):
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
 
-    def add_review(self, review):
+    def add_review(self, review: Review):
         """Add a review to the place."""
         self.reviews.append(review)
 
-    def add_amenity(self, amenity):
+    def add_amenity(self, amenity: Amenity):
         """Add an amenity to the place."""
         self.amenities.append(amenity)
