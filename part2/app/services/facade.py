6035ea944f0bdc12bea7b0872bb41713a9255409
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-23 14:52:56 +0100

    Added create_user, get_user and get_user_by_email methods in facade. Completed get_place method in facade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from app.persistence.repository import InMemoryRepository
+from app.models.place import Place, User
+
 
 class HBnBFacade:
     def __init__(self):
@@ -9,12 +11,19 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
-    def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+    ## Place
+    def get_place(self, place_id: str) -> Place:
+        return self.place_repo.get(place_id)
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+
+    ## User
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id: str) -> User:
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email: str) -> User:
+        return self.user_repo.get_by_attribute('email', email)
