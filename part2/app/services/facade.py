a051a02a0aac23ea9ba895c06a8221e1a741fde1
elliot <charlet.elliot@gmail.com>
2026-02-25 12:46:53 +0100

    end the route and the api calls for the amenities

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -3,6 +3,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.place import Place, User
 from app.models.review import Review
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
@@ -31,24 +32,20 @@ class HBnBFacade:
 
     ## Amenity
     def create_amenity(self, amenity_data):
-    # Placeholder for logic to create an amenity
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
 
         return amenity
 
     def get_amenity(self, amenity_id):
-    # Placeholder for logic to retrieve an amenity by ID
         return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-    # Placeholder for logic to retrieve all amenities
         return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-    # Placeholder for logic to update an amenity
         self.amenity_repo.update(amenity_id, amenity_data)
-
+        return self.amenity_repo.get(amenity_id)
 
     ## Review
 
