1792918e7920c2cec1f46fd67fc3794f5fce1dd6
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-25 14:26:19 +0100

    feat: Added reviews endpoints

---
+++ part2/app/api/v1/reviews.py
@@ -0,0 +1,94 @@
+from flask_restx import Namespace, Resource, fields
+from app.services import facade
+
+api = Namespace('reviews', description='Review operations')
+
+# Define the review model for input validation and documentation
+review_model = api.model('Review', {
+    'text': fields.String(required=True, description='Text of the review'),
+    'rating': fields.Integer(required=True, description='Rating of the place (1-5)'),
+    'user_id': fields.String(required=True, description='ID of the user'),
+    'place_id': fields.String(required=True, description='ID of the place')
+})
+
+@api.route('/')
+class ReviewList(Resource):
+    @api.expect(review_model)
+    @api.response(201, 'Review successfully created')
+    @api.response(400, 'Invalid input data')
+    def post(self):
+        """Register a new review"""
+        data = api.payload
+
+        if len(data['text']) <= 0:
+            return {'error': "Text cannot be empty"}, 400
+
+        if data['rating'] < 0 or data['rating'] > 5:
+            return {'error': 'Rating must be betweeen 0 and 5'}
+
+        review = facade.create_review(data)
+        return {
+            'id': review.id,
+            'text': review.text,
+            'rating': review.rating,
+            'user_id': review.user.id,
+            'place_id': review.place.id
+        }, 201
+
+    @api.response(200, 'List of reviews retrieved successfully')
+    def get(self):
+        """Retrieve a list of all reviews"""
+        result = []
+        reviews = facade.get_all_reviews()
+        for review in reviews:
+            result.append({
+                'id': review.id,
+                'text': review.text,
+                'rating': review.rating,
+                'user_id': review.user.id,
+                'place_id': review.place.id
+            })
+
+        return result
+
+@api.route('/<review_id>')
+class ReviewResource(Resource):
+    @api.response(200, 'Review details retrieved successfully')
+    @api.response(404, 'Review not found')
+    def get(self, review_id):
+        """Get review details by ID"""
+        review = facade.get_review(review_id)
+        if review:
+            return {
+                'id': review.id,
+                'text': review.text,
+                'rating': review.rating,
+                'user_id': review.user.id,
+                'place_id': review.place.id
+            }
+
+        reviews = facade.get_reviews_by_place(review_id)
+        return [{
+            'id': review.id,
+            'text': review.text,
+            'rating': review.rating,
+            'user_id': review.user.id,
+            'place_id': review.place.id
+        } for review in reviews]
+
+    @api.expect(review_model)
+    @api.response(200, 'Review updated successfully')
+    @api.response(404, 'Review not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, review_id):
+        """Update a review's information"""
+        data = api.payload
+        facade.update_review(review_id, data)
+
+    @api.response(200, 'Review deleted successfully')
+    @api.response(404, 'Review not found')
+    def delete(self, review_id):
+        """Delete a review"""
+        facade.delete_review(review_id)
+
+        return {'message': 'Review deleted'}
