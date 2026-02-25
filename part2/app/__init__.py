a288be15dbd74281b3b7f4891f85e253f49c4213
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-25 14:25:52 +0100

    feat: Added reviews namespace to api

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-from flask_restx import Api
+from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.users import api as users_ns
+from flask_restx import Api
+from flask import Flask
+
 
 def create_app():
     app = Flask(__name__)
@@ -12,5 +14,6 @@ def create_app():
     # Additional namespaces for places, reviews, and amenities will be added later
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
