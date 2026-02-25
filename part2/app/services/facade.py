aa8e6e922c8a62d6713e430d92ece8caec0e63de
elliot <charlet.elliot@gmail.com>
2026-02-25 07:06:36 +0100

    append the facade amenity

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -32,19 +32,22 @@ class HBnBFacade:
     ## Amenity
     def create_amenity(self, amenity_data):
     # Placeholder for logic to create an amenity
-        pass
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+
+        return amenity
 
     def get_amenity(self, amenity_id):
     # Placeholder for logic to retrieve an amenity by ID
-        pass
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
     # Placeholder for logic to retrieve all amenities
-        pass
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
     # Placeholder for logic to update an amenity
-        pass
+        self.amenity_repo.update(amenity_id, amenity_data)
 
 
     ## Review
