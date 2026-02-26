3e84fceb381681e5e13355d984d4be8c549c7bd9
Ilan <ilan.deversenne@holbertonstudents.com>
2026-02-26 09:50:02 +0100

    Added places namespace

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,7 @@
 
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from flask_restx import Api
 from flask import Flask
@@ -14,6 +15,7 @@ def create_app():
     # Additional namespaces for places, reviews, and amenities will be added later
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
