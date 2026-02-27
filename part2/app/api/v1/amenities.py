13f65f17fd7b8edee43d9b042981d9883da72193
elliot <charlet.elliot@gmail.com>
2026-02-27 10:46:49 +0100

    amenities get a bonus test in the put on the title

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -48,6 +48,8 @@ class AmenityResource(Resource):
     def put(self, amenity_id):
         """Update an amenity's information"""
         amenity_data = api.payload
+        if not amenity_data.name or len(amenity_data.name) == "":
+            return {'error': 'amenity must have a name even a bad name'}
         amenity = facade.update_amenity(amenity_id, amenity_data)
 
         return {'id': amenity.id, 'name': amenity.name}, 200
