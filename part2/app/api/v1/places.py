a2bc5b2e1b7a32e4eecadaeb5f013a84767585d1
elliot <charlet.elliot@gmail.com>
2026-02-27 10:54:48 +0100

    append the test to the put

--- part2/app/api/v1/places.py
+++ part2/app/api/v1/places.py
@@ -118,5 +118,15 @@ class PlaceResource(Resource):
     def put(self, place_id):
         """Update a place's information"""
         place_data = api.payload
+        if not place_data.title or len(place_data.title) < 3:
+            return {'error': 'a place need a good title'}, 400
+        if not isinstance(place_data.price, (float, int)) or place_data.price < 0:
+            return {'error': 'price must be a positive float or integer'}, 400
+        if place_data.latitude < -90 or place_data.latitude > 90:
+            return {'error': 'latitude must be float or int between -90 and 90'}, 400
+        if place_data.longitude < -180 or place_data.longitude > 180:
+            return {'error': 'longitude must be float or int between -180 and 180'}, 400
+        if not isinstance(place_data.owner, user_model):
+            return {'error': 'owner must be like the model'}, 400
         place = facade.update_place(place_id, place_data)
         return place
