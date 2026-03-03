5e506c8ecaa655f21e0f9911c1c8a7ef5dbcf851
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-03 23:00:01 +0100

    moved check into review model

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -3,6 +3,7 @@
 from app.models.base_model import BaseModel
 from app.models.place import Place, User
 
+from app.api.exceptions import BadRequest
 
 class Review(BaseModel):
     def __init__(self, text: str, rating: int, place: Place, user: User):
@@ -11,3 +12,25 @@ class Review(BaseModel):
         self.rating = rating
         self.place = place
         self.user = user
+
+    @property
+    def text(self):
+        return self.__text
+
+    @text.setter
+    def text(self, value: str):
+        if len(value.strip()) < 8:
+            raise BadRequest('Invalid input data')
+
+        self.__text = value
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, value: int):
+        if value < 1 or value > 5:
+            raise BadRequest('Invalid input data')
+
+        self.__rating = value
