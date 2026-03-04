8a369ff1c03373788a492a2342340720a494fb35
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-04 11:31:49 +0100

    added check into facade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -126,15 +126,23 @@ class HBnBFacade:
         return amenity
 
     def get_amenity(self, amenity_id):
-        return self.amenity_repo.get(amenity_id)
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            raise NotFound('Amenity not found')
+
+        return amenity
 
     def get_all_amenities(self):
         return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
         self.amenity_repo.update(amenity_id, amenity_data)
-        return self.amenity_repo.get(amenity_id)
 
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            raise NotFound('Amenity not found')
+
+        return amenity
 
     ## Review
 
